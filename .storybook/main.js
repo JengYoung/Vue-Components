6c1bf824f897ed74560762e33c5302fe7737d7a9
Jengyoung <young_pallete@naver.com>
2022-02-16 23:57:20 +0900

    chore: install eslint-config-prettier

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
+const path = require('path')
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  framework: "@storybook/vue3",
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  framework: '@storybook/vue3',
 };
---
+++ src/components/Base/Button.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="button" :style="buttonCSS">{{ children }}</button>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    children: String,
+    backgroundColor: String,
+    width: String,
+    height: String,
+    padding: String,
+  },
+  setup(props) {
+    const buttonCSS = computed(() => ({
+      backgroundColor: props.backgroundColor,
+      width: props.width,
+      height: props.height,
+      padding: props.padding,
+    }));
+    return {
+      buttonCSS,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
