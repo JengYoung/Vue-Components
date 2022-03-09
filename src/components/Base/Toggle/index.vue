52d680a0447170f79d92414ced8eb85bb8d35886
JengYoung <young_pallete@naver.com>
2022-03-09 22:58:32 +0900

    feat: Toggle Component 기본 props 구현

---
+++ src/components/Base/Toggle/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :style="toggleStyle" class="toggle">
+    <div class="toggle__button"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    width: String || Number,
+    height: String || Number,
+    backgroundColor: String,
+    buttonColor: String,
+    isToggle: Boolean,
+    border: {
+      type: String || Number,
+      default: '1px solid lightgray'
+    }
+  },
+  setup (props) {
+    const toggleStyle = {
+      backgroundColor: props.backgroundColor,
+      width: props.width,
+      height: props.height,
+      border: props.border
+    }
+    const toggleButtonStyle = {
+      backgroundColor: props.buttonColor
+    }
+
+    return {
+      toggleStyle,
+      toggleButtonStyle
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
