12f4372a5c65195827238b53fcbad0cf0a7f2d4f
JengYoung <young_pallete@naver.com>
2022-06-13 19:21:56 +0900

    :sparkles: Input 생성 및 v-model 적용

---
+++ src/components/Base/Input/FormattingInput.vue
@@ -0,0 +1,39 @@
+<template>
+  <input @input="changeInputValue">
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  props: {
+    modelValue: {
+      type: String,
+      required: true
+    },
+    blocks: {
+      type: Array as PropType<number[]>,
+      required: true
+    },
+    delimeter: {
+      type: String
+    },
+    prefix: {
+      type: String
+    }
+  },
+  setup (props, { emit }) {
+    const changeInputValue = (e: Event) => {
+      emit('update:inputValue', (e.target as HTMLInputElement).value); // 이전에는 `this.$emit('input', title)`
+    }
+
+    return {
+      changeInputValue
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
