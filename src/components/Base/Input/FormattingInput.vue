f0018d1223faf5beb4e63acf0ee741f0d0ec8c8a
JengYoung <young_pallete@naver.com>
2022-08-13 23:05:08 +0900

    :sparkles: block 변화에 따른 값 적용, delimeter 추가 로직 생성
    nextTick을 주어 먼저 DOM에서의 마운트를 끝낸 다음 계산할 수 있도록 하였다.

--- src/components/Base/Input/FormattingInput.vue
+++ src/components/Base/Input/FormattingInput.vue
@@ -4,7 +4,6 @@
   <div>blocks: {{ blocks }}</div>
   <div>delimeter: {{ delimeter }}</div>
   <div>modelValue: {{ modelValue }}</div>
-  <div>now inputValue: {{ value }}</div>
   <div>arr: {{ JSON.stringify(arr) }}</div>
   <div>inputDirection: {{ inputDirection }}</div>
   <div>selectionStart: {{ selectionStart}}</div>
@@ -13,11 +12,26 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, PropType, ref, watch } from 'vue'
+import { computed, defineComponent, nextTick, PropType, ref } from 'vue'
 
 enum InputDirectionEnum {
   PLUS = 'plus',
   MINUS = 'minus'
+}
+
+const reassignDelemeter = (nowValue: string, blocks: number[], delimeter: string) => {
+  let result = '';
+  let blocksIndex = 0;
+
+  for (let i = 0;  i < nowValue.length; i += 1) {
+    result += nowValue[i];
+    if (i === blocks[blocksIndex] - 1 && i < nowValue.length - 1) {
+      result += delimeter;
+      blocksIndex += 1;
+    }
+  }
+
+  return result
 }
 
 export default defineComponent({
@@ -31,7 +45,8 @@ export default defineComponent({
       required: true
     },
     delimeter: {
-      type: String
+      type: String,
+      required: true
     },
     prefix: {
       type: String
@@ -39,22 +54,53 @@ export default defineComponent({
   },
   setup (props, { emit }) {
     const inputElement = ref<HTMLInputElement | null>(null);
-    const value = ref('');
-    const computedModelValue = computed(() => props.modelValue);
-    const computedBlocks = computed(() => props.blocks);
+    const refinedBlocks = computed(() => {
+      // eslint-disable-next-line
+      const arr: any[] = [];
+
+      nextTick(() => {
+        props.blocks.forEach((val, idx) => {
+          arr.push(val + (idx ? arr[idx - 1] : 0))
+        })
+      })
+
+      return arr;
+    })
+
     const arr = ref<string[]>([]);
     const inputDirection = ref<InputDirectionEnum>(InputDirectionEnum.PLUS);
     const selectionStart = ref(0);
     const selectionEnd = ref(0);
 
-    const onInput = (e: Event) => {
-      emit('update:modelValue', (e.target as HTMLInputElement).value)
+    /**
+     *
+     * 1. selectionStart를 구한다.
+     * 2. selectionStart의 업데이트 값을 구한다.
+     *
+     * 결국 2번에 대한 방법을 찾아야 함.
+     * 1. 이전의 selectionStart값 앞에 있는 `-` 개수를 찾는다.
+     * 2. 이후의 selectionStart값 앞에 있는 `-` 개수를 찾는다.
+     * 만약 `-` 개수가 변화되었다면 그 개수만큼 업데이트를 해준다.
+     * 이를 함수화하여, 좀 더 선언적으로 관리한다.
+     */
+
+    const onInput = () => {
+      if (!inputElement.value) return;
+
+      let result = '';
+      const inputValue = inputElement.value.value;
+      const refinedValue = inputValue.replace(/[^0-9]/g, '').slice(0, 11);
+
+      result = reassignDelemeter(refinedValue, refinedBlocks.value, props.delimeter);
+
+      console.log(inputValue, props.modelValue, result)
+      inputElement.value.value = result;
+      emit('update:modelValue', result)
     }
 
 
     return {
       inputElement,
-      value,
       arr,
       inputDirection,
       selectionStart,
