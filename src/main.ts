c7ed7476a0322c1a2f08bbd980c39d0d25b2f712
JengYoung <young_pallete@naver.com>
2022-11-12 19:12:58 +0900

    :recycle: 글로벌 SCSS 변수를 js에서도 다룰 수 있도록 한다
    - sass-loader의 경우, 10버전 이상 설치 시 제대로 작동되지 않는 문제가 있으니 주의하자.
    - scss의 export를 통해 객체 상태로 가져온다.
    - 이를 순회하면서, `-`를 기준으로 키 값을 분리해낸다.
    - 키값에 value를 넣음으로써 scss 변수 객체를 만들어냈다.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+
 import "@css/normalize.css";
-import "@css/global.scss"
+import "@/css/global.scss"
+
+import globalCSS from './utils/globalCSS';
+
+console.log(globalCSS)
 
 createApp(App).use(store).use(router).mount('#app');
---
+++ src/utils/globalCSS.ts
@@ -0,0 +1,22 @@
+import scssVars from "@css/global.scss";
+
+interface GlobalCSSInterface {
+  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+  [key: string]: any;
+}
+
+const globalCSS: GlobalCSSInterface = {};
+
+Object.entries(scssVars).forEach(([key, value]) => {
+  const dashIndex = key.indexOf('-');
+  if (dashIndex < 0) {
+    globalCSS[key] = value;
+  } else {
+    const prefix = key.slice(0, dashIndex);
+    const cssVar = key.slice(dashIndex + 1);
+    globalCSS[prefix] = globalCSS[prefix] ?? {};
+    globalCSS[prefix][cssVar] = value;
+  }
+})
+
+export default globalCSS
