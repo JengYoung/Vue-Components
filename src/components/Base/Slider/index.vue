6ebe2547af30dc21c3e24c344e10682bb4e9410c
JengYoung <young_pallete@naver.com>
2022-11-22 15:38:19 +0900

    :recycle: 슬라이더 컴포넌트를 여러 방식으로 변경할 수 있도록 리팩토링한다

--- src/components/Base/Slider/index.vue
+++ src/components/Base/Slider/index.vue
@@ -1,20 +1,23 @@
 <template>
   <div class="slider" ref="sliderRef">
-    <div class="slider__inner" />
-    <div class="slider__track" :style="{ width: `${value}%` }" />
-    <div
-      class="slider__handle"
-      @mousedown="onDragStart"
-      @touchstart="onDragStart"
-      :style="{ left: `calc(${value}% - 6px)` }"
-    />
+    <div class="slider__inner">
+      <div class="slider__rail" />
+      <div class="slider__track" />
+      <div
+        class="slider__handle"
+        @mousedown="onDragStart"
+        @touchstart="onDragStart"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import globalCSS from '@/utils/globalCSS';
 import { ref, computed, watch, defineComponent } from 'vue';
 
 export default defineComponent({
+  emits: ['update:slider-value'],
   name: 'DefaultSlider',
   props: {
     max: {
@@ -29,13 +32,60 @@ export default defineComponent({
       type: Number,
       required: true,
     },
-    defaultValue: Number,
+    value: {
+      type: Number,
+      default: 0,
+    },
+    width: {
+      type: String,
+      default: '100%',
+    },
+    height: {
+      type: String,
+      default: '1rem',
+    },
+    railColor: {
+      type: String,
+      default: globalCSS.color.sub,
+    },
+    trackColor: {
+      type: String,
+      default: globalCSS.color.default,
+    },
+    handleSize: {
+      type: String,
+      default: '16px',
+    },
+    handleColor: {
+      type: String,
+      default: globalCSS.color.white,
+    },
+    handleActiveColor: {
+      type: String,
+      default: globalCSS.color.default,
+    },
+    padding: {
+      type: String,
+      default: '0',
+    },
+    backgroundColor: {
+      type: String,
+      default: 'transparent',
+    },
+    border: {
+      type: String,
+      default: 'none',
+    },
+    borderRadius: {
+      type: String,
+      default: '0px',
+    },
   },
-  setup(props) {
+  setup(props, { emit }) {
     const sliderRef = ref<HTMLDivElement | null>(null);
     const dragging = ref<boolean>(false);
 
-    const value = ref<number>(props.defaultValue || 0);
+    const value = ref<number>(props.value || 0);
     const percentage = computed(
       () => (value.value - props.min) / (props.max - props.min)
     );
@@ -70,6 +120,8 @@ export default defineComponent({
         props.min,
         props.max
       );
+
+      emit('update:slider-value', value.value);
     };
 
     const onTouchStart = (e: TouchEvent) => {
@@ -86,6 +138,8 @@ export default defineComponent({
         props.min,
         props.max
       );
+
+      emit('update:slider-value', value.value);
     };
 
     watch(
@@ -106,7 +160,15 @@ export default defineComponent({
       { deep: true }
     );
 
-    return { dragging, value, sliderRef, percentage, onDragStart, onMouseMove };
+    return {
+      dragging,
+      sliderValue: value,
+      sliderRef,
+      percentage,
+      onDragStart,
+      onMouseMove,
+      globalCSS,
+    };
   },
 });
 </script>
@@ -114,40 +176,68 @@ export default defineComponent({
 <style lang="scss" scoped>
 %slider-rails {
   position: absolute;
-  top: 0.375rem;
   left: 0;
+  align-self: center;
 
   height: 0.25rem;
+
   border-radius: 5px;
 }
 
 .slider {
-  position: relative;
-  width: 100%;
-  height: 1rem;
-  background: yellow;
+  width: v-bind('width');
+  height: v-bind('height');
+  padding: v-bind('padding');
+
+  cursor: pointer;
+  background-color: v-bind('backgroundColor');
+  border: v-bind('border');
+  border-radius: v-bind('borderRadius');
 
   &__inner {
+    position: relative;
+    display: flex;
     width: 100%;
-    background-color: skyblue;
+    height: v-bind('height');
+  }
+  &__rail {
+    align-self: center;
+    width: 100%;
+    background-color: v-bind('railColor');
+
     @extend %slider-rails;
   }
 
   &__track {
-    width: 0;
-    background-color: black;
+    align-self: center;
+    width: calc(v-bind('sliderValue') * 1%);
+    background-color: v-bind('trackColor');
+
     @extend %slider-rails;
   }
 
   &__handle {
     position: absolute;
-    top: 0.125rem;
-    left: 0;
-    z-index: 1110;
-    width: 0.75rem;
-    height: 0.75rem;
-    background-color: red;
+    left: calc(v-bind('sliderValue') * 1% - v-bind('handleSize') / 2);
+    z-index: 99;
+
+    box-sizing: border-box;
+    align-self: center;
+
+    width: v-bind('handleSize');
+    height: v-bind('handleSize');
+
+    background-color: v-bind('handleColor');
+
+    border: 1px solid v-bind('railColor');
     border-radius: 50%;
+    transition: background-color 0.2s;
+
+    &:hover,
+    &:active {
+      background-color: v-bind('handleActiveColor');
+      border: 1px solid v-bind('handleActiveColor');
+    }
   }
 }
 </style>
